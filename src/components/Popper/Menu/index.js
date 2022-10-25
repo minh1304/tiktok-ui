a3479fe88c5b6e0bf3a88f41b7650a8ef671c6a8
minh1304 <vtm1304@gmail.com>
2022-10-25 23:57:41 +0700

    UI header tt

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,25 +1,43 @@
 import Tippy from '@tippyjs/react/headless';
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Menu.module.scss';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import MenuItem from './MenuItem';
+import Header from './Header';
+
 const cx = classNames.bind(styles);
 // import MenuItem from './MenuItem';
 
 function Menu({ children, items = [] }) {
+    const [history, setHistory] = useState([{ data: items }]);
+    const current = history[history.length - 1];
     const renderItems = () => {
-        return items.map((item,index) => (
-            <MenuItem key={index} data={item}/>
-        ))
-    }
+        return current.data.map((item, index) => {
+            const isParent = !!item.children;
+            return (
+                <MenuItem
+                    key={index}
+                    data={item}
+                    onClick={() => {
+                        if (isParent) {
+                            setHistory(prev => [...prev, item.children]);
+                        }
+                    }}
+                />
+            );
+        });
+    };
     return (
         <Tippy
+            visible
             interactive
-            delay={[0,600]}
+            delay={[0, 600]}
             placement="bottom-end"
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper> 
+                    <PopperWrapper>
+                        {history.length > 1 && <Header title="Language" />}
                         {renderItems()}
                     </PopperWrapper>
                 </div>
