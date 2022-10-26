902c2f2d2d42123b21a83e8323f715a325ecbeca
minh1304 <vtm1304@gmail.com>
2022-10-26 20:19:40 +0700

    UI header tt

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -33,9 +33,9 @@ function Menu({ children, items = [], onChange = defaultFn }) {
     };
     return (
         <Tippy
-            visible
             interactive
             delay={[0, 600]}
+            offset={[16, 8]}
             placement="bottom-end"
             render={(attrs) => (
                 <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
@@ -52,7 +52,7 @@ function Menu({ children, items = [], onChange = defaultFn }) {
                     </PopperWrapper>
                 </div>
             )}
-            onHide = {()=> setHistory(prev => prev.slice(0,1))}
+            onHide={() => setHistory((prev) => prev.slice(0, 1))}
         >
             {children}
         </Tippy>
