c90e1e5ee813037ed99f38bb22b64ef71ff00e9d
minh1304 <vtm1304@gmail.com>
2022-11-03 02:28:12 +0700

    update scss

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -97,7 +97,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to='/'><img src={images.logo} alt="Tiktok" /></Link>
+                <Link to='/' className={cx('logo')}><img src={images.logo} alt="Tiktok" /></Link>
                 
 
                 <Search/>
