9bbf6e45f885b9d4a625da25442b1478b288b7da
minh1304 <vtm1304@gmail.com>
2022-11-24 13:19:25 +0700

    add play Background Video

--- src/layouts/components/Card/Card.js
+++ src/layouts/components/Card/Card.js
@@ -2,17 +2,35 @@ import classNames from 'classnames/bind';
 import styles from './Card.module.scss';
 import Button from '~/components/Button';
 import Image from '~/components/Image';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 function Card({ data }) {
+    const [play, setPlay] = useState(false);
     const handleFollow = (e) => {
-        e.preventDefault()
-        alert('Please Login!')
+        e.preventDefault();
+        alert('Please Login!');
+    };
+    const handlePlayCard = () => {
+        setPlay(true);
+    };
+    const handleStopPlayCard = () => {
+        setPlay(false)
     }
     return (
         <div className={cx('userCard')}>
             <a href={`/@${data.nickname}`} className={cx('aUserCard')}>
-                <div className={cx('divContainer')}>
+                <div className={cx('divContainer')} onMouseOver={handlePlayCard} onMouseOut={handleStopPlayCard}>
+                    {play &&
+                        <video
+                            autoPlay
+                            loop
+                            muted
+                            style={{ position: 'absolute', width: '100%', height: '100%', objectFit: 'cover' }}
+                        >
+                            <source src={data.popular_video.file_url} type="video/mp4" />
+                        </video>
+                    }
                     <Image className={cx('imgPoster')} src={data.popular_video.thumb_url} alt={data.nickname} />
                 </div>
                 <div className={cx('divInfoContainer')}>
@@ -24,8 +42,9 @@ function Card({ data }) {
                     </h5>
                     <h6 className={cx('nickname')}>{data.nickname}</h6>
                     <div className={cx('buttonContainer')}>
-                        <Button onClick={handleFollow} primary className={cx('buttonFollow')}>Follow</Button>
-
+                        <Button onClick={handleFollow} primary className={cx('buttonFollow')}>
+                            Follow
+                        </Button>
                     </div>
                 </div>
             </a>
